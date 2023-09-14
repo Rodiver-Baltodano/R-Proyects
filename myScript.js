bd8158b72df6fedba6cda0c0ca199627ee3cdb54
Rodiver-Baltodano <s.balto025@gmail.com>
2023-09-14 11:14:42 -0600

    update

---
+++ myScript.js
@@ -0,0 +1,60 @@
+
+function oc1() {
+    document.getElementById("fondo1").style.display = "none";
+    document.getElementById("fondo_cara1").style.display = "block";
+  }
+  function ms1()  {
+    document.getElementById("fondo1").style.display = "block";
+    document.getElementById("fondo_cara1").style.display = "none";
+}
+/*-----------------------------------------------------------------*/
+function oc2() {
+    document.getElementById("fondo2").style.display = "none";
+    document.getElementById("fondo_cara2").style.display = "block";
+  }
+  function ms2()  {
+    document.getElementById("fondo2").style.display = "block";
+    document.getElementById("fondo_cara2").style.display = "none";
+}
+/*-----------------------------------------------------------------*/
+function barraNavegacionDes() {
+    document.getElementById("navegacion").style.display = "none";
+    document.getElementById("forma2").style.display = "block";
+    document.getElementById("forma1").style.display = "none";
+    document.getElementById("nose").style.display = "none";
+  }
+function barraNavegacionApa()  {
+    document.getElementById("navegacion").style.display = "block";
+    document.getElementById("forma1").style.display = "block";
+    document.getElementById("forma2").style.display = "none";
+    document.getElementById("nose").style.display = "block";
+}
+
+function panel_visitenos() {
+  document.getElementById("navegacion").style.display = "block";
+}
+function panel_visitenos()  {
+  document.getElementById("").style.display = "none";
+}
+/*-----------------------------------------------------------------*/
+function lista_trabajo1()    {
+  document.getElementById("secciones-trabajo").style.display = "block";
+  document.getElementById("boton1").style.display = "none";
+  document.getElementById("boton2").style.display = "block";
+}
+function lista_trabajo2()    {
+  document.getElementById("secciones-trabajo").style.display = "none";
+  document.getElementById("boton1").style.display = "block";
+  document.getElementById("boton2").style.display = "none";
+}
+/*-----------------------------------------------------------------*/
+function visitenos1()    {
+  document.getElementById("lista_contacto").style.display = "block";
+  document.getElementById("visi1").style.display = "none";
+  document.getElementById("visi2").style.display = "block";
+}
+function visitenos2()    {
+  document.getElementById("lista_contacto").style.display = "none";
+  document.getElementById("visi2").style.display = "none";
+  document.getElementById("visi1").style.display = "block";
+}
